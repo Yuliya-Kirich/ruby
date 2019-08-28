7eef75fb8fdebf75ff048ec162c9d3574fc4d520
Yuliya-Kirich <40795656+Yuliya-Kirich@users.noreply.github.com>
2019-08-28 16:42:27 +0300

    hot-fix for 2 task

--- 2.5.rb
+++ 2.5.rb
@@ -4,41 +4,41 @@
 # (Запрещено использовать встроенные в ruby методы для этого вроде Date#yday или Date#leap?)
 # Алгоритм опредления високосного года: www.adm.yar.ru
 
+puts "Введите год: "
+number_year = gets.chomp.to_i
+puts "Введите месяц (ввод согласно названиям из списка:
+January
+February
+March
+April
+May
+June
+July
+August
+September
+October
+November
+December"
+name_month = gets.chomp
+puts "Введите день (число): "
+number_day = gets.chomp.to_i
+year = {1 => {January: 31}, 2 => {February: 28}, 3 => {March: 31}, 4 => {April: 30}, 5 => {May: 31}, 6 => {June: 30}, 7 => {July: 31},
+        8 => {August: 31}, 9 => {September: 30}, 10 => {October: 31}, 11 => {November: 30}, 12 => {December: 31}}
+search_period_by_month = {}
+sum = 0
 
+year.each {|number, my_hash_month| my_hash_month.each {|month, day|
+  if (number_year.to_i % 100 == 0) & (number_year.to_i % 400 == 0) || ((number_year.to_i % 100 != 0) & (number_year.to_i % 4 == 0))
+    year[2] = {:February => 29}
+   end
+  if month.to_s == name_month
+    search_period_by_month =  year.take(number-1)
+  end
+}
+}
 
+search_period_by_month.each {|number, my_hash_month| my_hash_month.each {|month, day| sum += day}}
 
-  a_index = 0
-  sum_day = 0
-  itog = 0
+puts "Порядковый номер даты, начиная отсчет с начала года: #{sum + number_day}"
 
-  year = {{:1 => {January: 31} }, {:2 => {February: 28}}, {:3 => {March: 31}}, {:4 => {April: 30}}, {May: 31}, {June: 30}, {July: 31}, {August: 31}, {September: 30}, {October: 31}, {November: 30}, {December: 31}}
-
-  puts "Введите год: "
-  number_year = gets.chomp.to_i
-  puts "Введите месяц (ввод согласно названиям из списка: January, February, March, April, May, June, July, August, September, October, November, December): "
-  number_month = gets.chomp
-  puts "Введите день (число): "
-  number_day = gets.chomp.to_i
-
-if (number_year.to_i % 100 == 0) & (number_year.to_i % 400 == 0)
-  year[1] = {"February" => 29}
-elsif (number_year.to_i % 100 != 0) & (number_year.to_i % 4 == 0)
-  year[1] = {"February" => 29}
-end
-
-  year.each {|elem| elem.each {|key, value|
-if key == number_month
-  a_index = year.index(elem)
-end
-  }
-  }
-
-  year.each { |number|
-  s = year.index(number)
-unless s > (a_index-1)
-  number.each {|key, value_k| sum_day += value_k }
-end
-  itog = sum_day.to_i + number_day.to_i
-  }
-  puts "Порядковый номер введенной даты: #{itog}"
 
--- 2.6.rb
+++ 2.6.rb
@@ -5,46 +5,39 @@
 # Также вывести итоговую сумму за каждый товар.
 # Вычислить и вывести на экран итоговую сумму всех покупок в "корзине".
 
+# Это в блокноте на винде прогоняю, когда в дороге. Пусть тут останется, пожалуйста.
+# if (Gem.win_platform?)
+#   Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+#   Encoding.default_internal = __ENCODING__
+#
+#   [STDIN, STDOUT].each do |io|
+#     io.set_encoding(Encoding.default_external, Encoding.default_internal)
+#   end
+# end
 
-
-if (Gem.win_platform?)
-  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
-  Encoding.default_internal = __ENCODING__
-
-  [STDIN, STDOUT].each do |io|
-    io.set_encoding(Encoding.default_external, Encoding.default_internal)
-  end
-end
-
-
-  purchases = {}
-  price_and_quantity = {}
-  total_amount = []
-  total_amount_of_goods = {}
-  sum = 0
+purchases = {}
+sum = 0
 
 loop do
   puts "Введите название товара :"
   product = gets.chomp.to_s
-
-if  (product != "стоп".to_s) & (product != "stop")
-
-  puts "Введите цену за единицу товара : "
-  unit_price = gets.chomp
-  puts "Введите количество купленного товара : "
-  quantity_of_goods = gets.chomp
-
-  purchases [product] = {unit_price => quantity_of_goods}
-#  total_amount << (unit_price.to_f * quantity_of_goods.to_f)
-  total_amount_of_goods [product] = (unit_price.to_f * quantity_of_goods.to_f)
-end
-
-break if  (product == "стоп") || (product == "stop")
-end
-
-  puts purchases
-  total_amount_of_goods.each do |name_product, total|
-  puts "Итоговая сумма по товару #{name_product} : #{total}"
-  sum += total
+  if (product != "стоп".to_s) & (product != "stop")
+    puts "Введите цену за единицу товара : "
+    unit_price = gets.chomp.to_f
+    puts "Введите количество купленного товара : "
+    quantity_of_goods = gets.chomp.to_f
+    purchases[product] = {unit_price => quantity_of_goods}
   end
-  puts "Итоговая сумма всех покупок в корзине: #{sum}"
+  break if (product == "стоп") || (product == "stop")
+end
+
+puts purchases
+
+purchases.each do |product_t, amount_of_goods|
+  amount_of_goods.each do |unit_price, quantity_of_goods|
+    sum += (unit_price * quantity_of_goods)
+    puts "Итоговая сумма по товару #{product_t} : #{unit_price * quantity_of_goods}"
+  end
+end
+
+puts "Итоговая сумма всех покупок в корзине: #{sum}"
