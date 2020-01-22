947792a8fda1941f9f535be7671090bb05c98d1c
Yuliya-Kirich <yvkirich@gmail.com>
2020-01-23 00:59:13 +0300

    block3

--- 8 issue/station.rb
+++ 8 issue/station.rb
@@ -33,54 +33,59 @@ class Station
 
   end
 
-
   def add_train(train)
     #@trains_at_station << train
     key = (@trains_at_station.length + 1)
     @trains_at_station[key] = train
+    # add_train_in_station {|value| puts "Номер поезда - #{value.number},тип - #{value.type}, кол-во вагонов - #{value.carriages}" }
     add_train_in_station
   end
-
-
 
   def add_train_in_station
     puts @@all_station
     @@all_station.each do |key, station|
-      puts "все параметры класса #{station.methods} наименование станции -'#{key}'"
-      puts  "поезда на станции #{station.trains_at_station}"
-      station.trains_at_station.each do |key_number, value|
-        #if value == :trains_at_station
+      #  puts "все параметры класса #{station.methods} наименование станции -'#{key}'"
+      #  puts "поезда на станции #{station.trains_at_station}"
+        station.trains_at_station.each do |key_number, value|
+          #if value == :trains_at_station
+          
+          # block  if value.class == PassengerTrain || value.class == CargoTrain
+
+           puts "найден !!! #{value.number}, #{value.type}, #{value.carriages.length}" if value.class == PassengerTrain
+           puts "найден !!! #{value.number}, #{value.type}, #{value.carriages.length}" if value.class == CargoTrain
+
+          
+          
+          # puts "найден !!! #{value.number}, #{value.type}, #{value.carriages.length}"
+          # number = value.number
+          # type = value.type
+          # carriages = value.carriages.length
+
+          # hj
+          # yield
+           #yield("#{value.number}", "#{value.type}", "#{value.carriages.length}")
+           #  block.call
+            end
+          # else
+          #  puts 'не найден метод'
 
 
+       # station.methods.each_pair {| key_value_array | block }
 
-        #if value.class == PassengerTrain || value.class == CargoTrain
+       # puts value.inspect
 
-         puts "найден !!! #{value.number}, #{value.type}, #{value.carriages.length}"
-         number = value.number
-         type = value.type
-         carriages = value.carriages.length
-         hj
-
-         yield("#{value.number}", "#{value.type}", "#{value.carriages.length}")
-
-          #end
-          #else
-          #    puts 'не найден метод'
-        #end
-
-     # station.methods.each_pair {| key_value_array | block }
-
-
-      end
       end
   end
 
+
+
   def hj
-    add_train_in_station do |number, type, carriages|
-      puts do
-        "Номер поезда - #{number},тип - #{type}, кол-во вагонов - #{carriages}"
-      end
-    end
+  #add_train_in_station do |number, type, carriages|
+  # puts do
+  #  "Номер поезда - #{number},тип - #{type}, кол-во вагонов - #{carriages}"
+  #  end
+  #  end
+  #block = proc { puts "найден !!! #{value.number}, #{value.type}, #{value.carriages.length}" }
   end
 
 
@@ -97,6 +102,7 @@ class Station
   def delete_train(train)
     @trains_at_station.delete(train)
   end
+
   def validate!
     station_validate
   end
